591c8176ae5c3da6049d3478d7384594b8644ce9
lilprof01 <adewumime@gmail.com>
2025-03-11 17:31:06 +0100

    almost done

--- src/Components/LandingComponents/Header.jsx
+++ src/Components/LandingComponents/Header.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header role='banner' className='flex justify-between items-center align-middle py-4 px-8 bg-white dark:bg-black w-full z-10 top-0 left-0'>
-      <h1 className='text-[#6C3BAA] font-bold text-2xl hover:cursor-pointer select-none'>LEXI</h1>
-      <div className='sm:flex gap-4 hidden'>
-        <Link to="/login" className='px-8 py-3 border border-[#6C3BAA] text-[#6c3baa] font-semibold rounded-full hover:cursor-pointer hover:bg-[#6c3baa] hover:text-white dark:text-white'>Log In</Link>
-        <Link to="/signup" className='px-8 py-3 bg-[#6C3BAA] text-white font-semibold rounded-full hover:cursor-pointer hover:opacity-85'>Sign Up</Link>
+    <header
+      role="banner"
+      className="flex justify-between items-center align-middle py-4 px-8 bg-white dark:bg-black w-full z-10 top-0 left-0"
+    >
+      <h1 className="text-[#6C3BAA] font-bold text-2xl hover:cursor-pointer select-none">
+        LEXI
+      </h1>
+      <div className="sm:flex gap-4 hidden">
+        <Link
+          to="/login"
+          className="px-8 py-3 border border-[#6C3BAA] text-[#6c3baa] font-semibold rounded-full hover:cursor-pointer hover:bg-[#6c3baa] hover:text-white dark:text-white"
+        >
+          Log In
+        </Link>
+        <Link
+          to="/signup"
+          className="px-8 py-3 bg-[#6C3BAA] text-white font-semibold rounded-full hover:cursor-pointer hover:opacity-85"
+        >
+          Sign Up
+        </Link>
+      </div>
+
+      {/* hamburger for mobile */}
+      <div
+        className={`hamburger h-10 w-10 flex flex-col justify-center items-end align-middle gap-1 z-[100] hover:cursor-pointer sm:hidden`}
+      >
+        <div className={`h-1 w-12 bg-[#121212] dark:bg-[#f6f4ef]`}></div>
+        <div className={`h-1 w-8 bg-[#121212] dark:bg-[#f6f4ef]`}></div>
+        <div className={`h-1 w-6 bg-[#121212] dark:bg-[#f6f4ef]`}></div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Components/LandingComponents/Testimonials.jsx
+++ src/Components/LandingComponents/Testimonials.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
 
 const Testimonials = () => {
   return (
-    <section role='testimonials' className='testimonials-section h-screen flex flex-col justify-center items-center bg-[#fe4d01] py-8'>
-      <h2 className='text-[#ffd966] font-bold text-3xl'>Testimonials</h2>
-      <div className='flex justify-between py-8 h-full w-[90%] gap-8 overflow-x-scroll'>
-        <div className='h-full min-w-[90vw] lg:min-w-[40vw] bg-amber-300'></div>
-        <div className='h-full min-w-[90vw] lg:min-w-[40vw] bg-amber-300'></div>
-      </div>
-      <div className='text-5xl text-amber-200 absolute right-5 hover:cursor-pointer'>&gt;</div>
-      <div className='text-5xl text-amber-200 absolute left-5 hover:cursor-pointer'>&lt;</div>
+    <section
+      role="testimonials"
+      className="testimonials-section h-screen flex flex-col justify-center items-center bg-[#fe4d01] py-8"
+    >
+      <h2 className="text-[#ffd966] font-bold text-3xl">Testimonials</h2>
+      <Swiper
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+        spaceBetween={5}
+        breakpoints={{
+          768: {
+            slidesPerView: 1,
+          },
+          1024: {
+            slidesPerView: 2,
+          }
+        }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="w-[80%] h-[80%]"
+      >
+        <SwiperSlide className="w-full h-[80%] bg-red-700 rounded-4xl flex justify-center items-center align-middle text-center">Slide 1</SwiperSlide>
+        <SwiperSlide className="w-full h-[80%] bg-red-700 rounded-4xl flex justify-center items-center align-middle text-center">Slide 2</SwiperSlide>
+        <SwiperSlide className="w-full h-[80%] bg-red-700 rounded-4xl flex justify-center items-center align-middle text-center">Slide 3</SwiperSlide>
+        <SwiperSlide className="w-full h-[80%] bg-red-700 rounded-4xl flex justify-center items-center align-middle text-center">Slide 4</SwiperSlide>
+      </Swiper>
     </section>
-  )
-}
+  );
+};
 
 export default Testimonials;
